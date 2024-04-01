2e5054d7250b10eee996a4d78d20fee1f13a0d31
Suchitra Shankar <144136955+SuchitraShankar07@users.noreply.github.com>
2024-04-01 15:55:26 +0530

    Add files via upload

--- casestudy.c
+++ casestudy.c
@@ -1,71 +1,128 @@
 #include <stdio.h>
 #include <strings.h>
 #include "functionmodule.c"
-int createaccount();
-int checkbalance();
-int depositmoney();
-int withdrawmoney();
+
+void createaccount();
+void checkbalance(int* pbal);
+void depositmoney(int* pbal);
+void withdrawmoney(int* pbal);
+void displaydetails();
+int t;
 
 int main(void)
-{
-	printf("WELCOME TO PES BANK!! ARE YOU?\n 1. a new customer\n 2. an existing customer\n");
-	int a;
-	scanf("%d",&a);
-	getchar();
-	switch(a)
-	{	
-	case 1:
+{	int bal=0;
+	int* pbal=&bal;
+	int bigt=1;
+	char rflag;
+	while (bigt==1)
 	{
-		printf("Would you like to make an account?(Y/N)");
-		char ch;
-		scanf("%c", &ch);
+		bigt=0;
+		printf("WELCOME TO PES BANK!! ARE YOU?\n 1. a new customer\n 2. an existing customer\n 3. Exit.\n");
+		int a;
+		scanf("%d",&a);
 		getchar();
-		if ((ch=='Y')|(ch=='y'))
-		{
-			//createaccount();
+		switch(a)
+		{	
+			case 1:
+			{
+				printf("Would you like to make an account?(Y/N)\n");
+				char ch;
+				scanf("%c", &ch);
+				getchar();
+				if ((ch=='Y')|(ch=='y'))
+				{
+					createaccount();
+					
+				}
+				else
+				{
+					printf("Till we meet again! See you soon!\n");
+					printf("-------------------------------------------------------------------------\n");
+					break;
+				}
+	
+				break;
+			}
+			case 2:
+			{
+
+				//printf()
+				//printf("Enter password");
+				//code for checking password to be implemented after files are introduced, as structs are required to assign passwords to each user.
+				t=1;
+				char flag;
+				while(t==1)
+				{
+					printf("WELCOME TO PES BANK!! CHOOSE YOUR PREFERRED ACTION:\n");
+					printf("1. Check my balance\n");
+					printf("2. Deposit money into my account\n");
+					printf("3. Make a withdrawal\n");
+					printf("4. Display my account details\n");
+					printf("Enter the serial number of the action you wish to perform\n");
+					int ser;
+					
+
+					scanf("%d%*c", &ser);
+					switch(ser)
+					{
+						
+						case 1:
+						{
+							checkbalance(pbal);
+							break;
+						}
+						case 2:
+						{
+							depositmoney(pbal);
+							break;
+						}
+						case 3:
+						{
+							withdrawmoney(pbal);
+							break;
+						}
+						case 4:
+						{
+							displaydetails();
+							break;
+						}
+						
+						default:
+						printf("Invalid input.\n Please choose an option between 1 and 6\n");
+					}
+			t=0;
+			printf("Do you wish to perform a different action as an existing customer?(Y/N)\n");
+			//may remove later, due to better functionalities to come
+			scanf(" %c%*c", &flag);
+				if(flag=='Y'||flag=='y')
+				{
+					t=1;
+				}
+		printf("-------------------------------------------------------------------------\n");
+			}
 			break;
 		}
-		else
-		{
-			printf("Till we meet again! See you soon!");
-			break;
-		}
-	}
-	case 2:{
-		printf("WELCOME TO PES BANK!! CHOOSE YOUR PREFERRED ACTION:\n");
-		printf("1. Check my balance\n");
-		printf("2. Deposit money into my account\n");
-		printf("3. Make a withdrawal\n");
-		printf("4. Exit the program\n");
-		printf("Enter the serial number of the action you wish to perform\n");
-		int ser;
-		scanf("%d", &ser);
-		switch(ser)
-		{
-		
-		case 1:
-		{
-			//checkbalance();
-			break;
-		}
-		case 2:
-		{
-			//depositmoney();
-			break;
-		}
+
 		case 3:
-		{
-			//withdrawmoney();
-			break;
-		}
-		case 4:
-		{
-			//exitprogram();
-			break;
-		}
+
+		break;
 		default:
-		printf("Invalid input.\n Please choose an option between 1 and 6\n");
-	}}}
-}
+		printf("Invalid input\n");
+		
+		}
 
-
+		
+		printf("Do you wish to do anything else in the bank?(Y/N)\n");
+			scanf(" %c%*c", &rflag);
+			if(rflag=='Y'||rflag=='y')
+			{
+				bigt=1;
+			}
+			else {
+			bigt=0;
+			}
+		printf("Thankyou for visiting, see you soon!");
+		printf("-------------------------------------------------------------------------\n");
+		}
+		return 0;
+		}
--- functionmodule.c
+++ functionmodule.c
@@ -1,3 +1,111 @@
-#include <string.h>
+
 #include <stdio.h>
-#include <stdlib.h>
+#include "curses.c"
+
+char username[50];
+char userid[10];
+char gender;
+int aadhar[12];
+unsigned age;
+char pass[10];
+
+void createaccount()
+{
+printf("Enter your name\n");
+fgets(username, 50, stdin);
+printf("Enter a password\n");
+
+
+for(int i=0;i<10;i++)
+    {
+        pass[i]=mygetch();
+        if(pass[i]=='\n')
+        {
+            pass[i]='\0';
+            break;
+        }
+        else
+        {
+            printf("*");
+        }
+}
+printf("\n");
+printf("KYC process:\n");
+printf("Enter your Gender(M/F/P),M = male, F = Female ,P= prefer not to say\n");
+scanf("%c", &gender);
+
+printf("Enter your age(in numbers)\n");
+scanf("%d",&age);
+printf(("Enter your Aadhar number( 12 digits, each digit in a new line):\n"));
+    for (int i = 0; i < 12; i++) {
+        scanf("%d", &aadhar[i]);}
+        
+printf("Thank you for making an account with PES Bank! Feel free to contact our branch manager for any assistance required.\n");
+printf("______________________________________________________________________________________________________________________________________________\n");
+}
+
+
+void displaydetails()
+{
+    printf("User name:%s\n", username);
+    //printf("userId:%s\n", userid); will be updated after structs are used
+    if (gender=='F'||gender=='f') {
+    printf("Gender: Female\n");
+    }
+    else if (gender=='M'||gender=='m') {
+        printf("Gender: Male\n");
+    }
+    else {
+    printf("Gender not disclosed\n");
+    }
+printf("Aadhar:");
+    for(int i=0; i<12; i++)
+    {
+        printf("%d", aadhar[i]);
+    }
+    printf("\nAge:%d\n", age);
+}
+
+void checkbalance(int* pbal)
+{
+printf("You have Rs.%d in your account\n", *pbal);
+}
+
+
+void depositmoney(int* pbal)
+{
+    int dep;
+printf("Enter amount to be deposited\n");
+scanf("%d", &dep);
+if (dep<=0)
+{
+    printf("Invalid amount\n");
+}
+else {
+*pbal+=dep;
+}
+printf("You now have Rs. %d in your account\n", *pbal);
+}
+
+
+void withdrawmoney(int* pbal)
+{
+    if(*pbal>0)
+    {
+        int with=0;
+        printf("How much money would you like to withdraw\n");
+        scanf("%d",&with);
+        if ((with<*pbal) && (with>0))
+        {
+            *pbal=*pbal-with;   
+            printf("%d withdrawn from account. \nYou now have Rs.%d left in your account\n", with, *pbal);
+        }
+        else 
+        {
+        printf("Insufficient funds :(\n");
+        }
+    }
+    else {
+    printf("You have zero balance in your account :(\n");
+    }
+}
